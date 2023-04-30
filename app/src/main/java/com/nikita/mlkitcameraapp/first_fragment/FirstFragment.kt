678b54ab4573a6e7e61e0f6052cc6e7dbf022c0f
Bogdan <112793886+pavyk-shnyk@users.noreply.github.com>
2023-04-30 17:47:40 +0600

    30.04.23

---
+++ app/src/main/java/com/nikita/mlkitcameraapp/first_fragment/FirstFragment.kt
@@ -0,0 +1,30 @@
+package com.nikita.mlkitcameraapp.first_fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.nikita.mlkitcameraapp.R
+
+class FirstFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.start_scan_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.start_scan)
+        button.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+    }
+}
